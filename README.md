973cb30748ee3b463369663c1f3e9de7deb41454
Calba breaker <calbabreaker@gmail.com>
2020-06-02 08:17:03 +0000

    added audio synth

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Welcome to The hlp.js library where there is bunch of useful tools.
 You will see different sections for the different parts to the library.
 
 ### Notes: 
-* The source code is in the src/ directory and all the paths are in src/paths.xtx
+* The source code is in the src/ directory
 * The library code you probaly want to use is in the lib/ directory with hlp.js and hlp.min.js (minified version)
 
 ## SRC files
@@ -11,6 +11,7 @@ These are the script tags for the lastest hlp.js
 ```html
 <script src="/src/main.js"></script>
 <script src="/src/dictionary.js"></script>
+<script src="/src/audioSynth.js"></script>
 
 <script src="/src/math/additions.js"></script>
 <script src="/src/math/matrix.js"></script>
---
+++ src/audioSynth.js
@@ -0,0 +1,54 @@
+class AudioPlayer {
+  constructor() {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    this.context = new AudioContext();
+    this.reset();
+  }
+
+  play(freq, vol, wave, when = 0) {
+    if (!this.hasResseted) return console.error("Make sure you reset the AudioPlayer before playing!");
+
+    if (freq != null) this.setFreq(freq);
+    if (wave != null) this.setWaveType(wave);
+    if (vol != null) this.setVolume(vol);
+    this.oscillator.start(when);
+
+    this.hasResseted = false;
+    return this;
+  }
+
+  stop(when = 0, useExpRamp = false) {
+    if (useExpRamp) {
+      this.gain.gain.setValueAtTime(this.gain.gain.value, this.context.currentTime);
+      this.gain.gain.exponentialRampToValueAtTime(0.00001, this.context.currentTime + when);
+    }
+
+    this.oscillator.stop(this.context.currentTime + when);
+    return this;
+  }
+
+  reset(filter = this.context.destination) {
+    this.gain = this.context.createGain();
+    this.gain.connect(filter);
+    this.oscillator = this.context.createOscillator();
+    this.oscillator.connect(this.gain);
+    this.compressor = this.context.createDynamicsCompressor();
+    this.compressor.connect(this.context.destination);
+    this.hasResseted = true;
+    return this;
+  }
+
+  setWaveType(wave) {
+    this.oscillator.type = wave;
+    return this;
+  }
+
+  setFreq(freq) {
+    this.oscillator.frequency.value = freq;
+  }
+
+  setVolume(vol) {
+    this.gain.gain.value = vol;
+    return this;
+  }
+}
