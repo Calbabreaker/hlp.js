aa1f94d4fc2bbb80f2ce6e64fdc9dc102f91632a
calbabreaker <calbabreaker@gmail.com>
2020-06-06 14:23:31 +0930

    added copy to clipboard

--- src/extra/strings.js
+++ src/extra/strings.js
@@ -42,3 +42,27 @@ hlp.onlyContainsSpaces = (str) => {
 hlp.containsNumbers = (str) => {
   return /[0-9]/g.test(str);
 };
+
+hlp.copyToClipboard = async (str) => {
+  if (!navigator.clipboard) {
+    // fallback for comapatipility
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    const successful = document.execCommand("copy");
+    if (!successful) throw new Error("Copy fallback not sucessful!");
+
+    document.body.removeChild(textArea);
+  } else {
+    await navigator.clipboard.writeText(str);
+  }
+};
