305b08b523d488d8e35cc18680263a1d09971a1d
calbabreaker <calbabreaker@gmail.com>
2020-06-13 18:04:49 +0930

    fixed httpPost

--- src/net/http.js
+++ src/net/http.js
@@ -1,18 +1,18 @@
-hlp.httpPost = (url, type, data, options = {}) => {
+hlp.httpPost = async (url, type, data, options = {}) => {
   const response = await fetch(url, {
-    method: 'POST', 
-    mode: options.mode || 'cors',
-    cache: options.cache || 'no-cache', 
-    credentials: options.credentials || 'same-origin', 
+    method: "POST",
+    mode: options.mode || "cors",
+    cache: options.cache || "no-cache",
+    credentials: options.credentials || "same-origin",
     headers: options.headers || {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    redirect: options.redirect || 'follow', 
-    referrerPolicy: options.referrerPolicy ||'no-referrer',
-    body: JSON.stringify(data) 
+    redirect: options.redirect || "follow",
+    referrerPolicy: options.referrerPolicy || "no-referrer",
+    body: JSON.stringify(data),
   });
-  
+
   if (type == hlp.json) return await response.json();
   else if (type == hlp.blob) return await response.blob();
   else return await response.text();
-}
+};
